92f48a78bb173c70d051737968c2a42ca328b2af
Locke Lemora <bermudezc.mario@gmail.com>
2022-09-01 11:58:06 -0500

    stage 5

--- RPS.js
+++ RPS.js
@@ -1,22 +1,32 @@
-const rpsArray = ['Rock', 'Paper', 'Scissor'];
+const rpsArray = ['Rock', 'Paper', 'Scissors'];
 let item = rpsArray[Math.floor(Math.random() * rpsArray.length)];
-const playerSelection = "Rock";
+let answer = prompt("Rock, Paper, Scissors ?");
+let playerSelection = answer;
 let computerSelection = item;
 let playerScore = 0;
 let computerScore = 0;
+
+function errorMessage(answer) {
+    if (answer != ("Rock" || "Paper" || "Scissors")) {
+        answer = prompt("Rock, Paper, Scissors ?, Write carefully!")
+    }
+    else {
+        answer = answer
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return ("Tie, nobody wins");
     } else if ( 
-        (playerSelection === 'Rock' && computerSelection === 'Scissor') ||
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
         (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-        (playerSelection === 'Scissor' && computerSelection === 'Paper')) {
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
         return ("Player wins");
     } else if (
-    (playerSelection === 'Scissor' && computerSelection === 'Rock') ||
+    (playerSelection === 'Scissors' && computerSelection === 'Rock') ||
     (playerSelection === 'Rock' && computerSelection === 'Paper') ||
-    (playerSelection === 'Paper' && computerSelection === 'Scissor')) {
+    (playerSelection === 'Paper' && computerSelection === 'Scissors')) {
     return ("Computer wins");
     }
 }
@@ -24,16 +34,27 @@ function playRound(playerSelection, computerSelection) {
 function game() {
     for (let i = 0; i < 5; i++) {
         if (playRound(playerSelection, computerSelection) === 'Player wins') {
-            console.log(playRound(playerSelection, computerSelection));
+            playerScore += 1
+            console.log("Player: " + playerScore + " | Computer: " + computerScore);
         } 
         else if (playRound(playerSelection, computerSelection) === 'Computer wins') {
-            console.log(playRound(playerSelection, computerSelection));
+            computerScore += 1
+            console.log("Player: " + playerScore + " | Computer: " + computerScore);
         }
         else if (playRound(playerSelection, computerSelection) === 'Tie, nobody wins') {
-            console.log(playRound(playerSelection, computerSelection));
+            
     }
-    item = rpsArray[Math.floor(Math.random() * rpsArray.length)];
-    computerSelection = item;
+        item = rpsArray[Math.floor(Math.random() * rpsArray.length)];
+        computerSelection = item;
+    }
+    if (playerScore > computerScore) {
+        console.log("Player Wins");
+    }
+    else if (playerScore < computerScore) {
+        console.log("Computer Wins");
+    }
+    else {
+        console.log("Tie game!")
     }
 }
 
